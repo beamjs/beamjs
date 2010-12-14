11a1dbe3be1a8cd8aa3c00d2d59d30961109fdba
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-13 18:16:57 -0800

    REPL will understand compilation errors now

--- src/beamjs_repl_console.erl
+++ src/beamjs_repl_console.erl
@@ -5,6 +5,8 @@
 read(Prompt) ->
 	io:get_line(Prompt).
 
+print({compilation_failed, Error}) ->
+	io:format("    Compilation failed: ~p~n",[Error]); 
 print({finished, Result}) ->
 	io:format("~p~n",[Result]);
 print({exception, Exception}) ->
