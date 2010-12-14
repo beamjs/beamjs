c14409bd1cbbc0d70be7250ed0b79937d777a062
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-13 18:04:12 -0800

    REPL workers are transient, not permanent now.

--- src/beamjs_repl_sup.erl
+++ src/beamjs_repl_sup.erl
@@ -9,7 +9,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Restart, Type), {I, {I, start_link, []}, Restart, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -24,6 +24,6 @@ start_link() ->
 
 init([]) ->
     {ok, { {simple_one_for_one, 5, 10}, [
-								  ?CHILD(beamjs_repl,worker)
+								  ?CHILD(beamjs_repl,transient,worker)
 								 ]} }.
 
