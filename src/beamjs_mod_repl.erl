36924cd5fc958ccf266ccfe1388cb5f0fa877bcf
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-19 02:20:40 -0800

    Make REPL show node name in the prompt if it is a distributed node.

--- src/beamjs_mod_repl.erl
+++ src/beamjs_mod_repl.erl
@@ -10,7 +10,13 @@ exports(_VM) ->
 	erlv8_object:new([{"start", fun start/2}]).
 
 start(#erlv8_fun_invocation{} = Invocation, []) ->
-	start(Invocation,["beam.js> "]);
+	case node() of
+		nonode@nohost ->
+			start(Invocation,["beam.js> "]);
+		Node ->
+			start(Invocation,[lists:flatten(io_lib:format("(~p)beam.js> ",[Node]))])
+	end;
+			
 start(#erlv8_fun_invocation{ vm = VM } = _Invocation, [Prompt]) ->
 	supervisor:start_child(beamjs_repl_sup,[Prompt, beamjs_repl_console, VM]),
 	receive X ->
