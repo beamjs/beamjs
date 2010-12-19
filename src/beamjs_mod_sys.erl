f022278765c6aa59e370b2feb864684d920e0992
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-19 02:09:46 -0800

    Added sys.beamjs.reloader() that reloads Beam.js

--- src/beamjs_mod_sys.erl
+++ src/beamjs_mod_sys.erl
@@ -4,11 +4,15 @@
 -include_lib("erlv8/include/erlv8.hrl").
 
 init(_VM) ->
-	ok.
+	reloader:start().
 
 exports(_VM) ->
-	?V8Obj([{"print", fun print/2}]).
+	?V8Obj([{"print", fun print/2},
+			{"beamjs", ?V8Obj([{"reload", fun reload/2}])}]).
 
 print(#erlv8_fun_invocation{ vm = VM} = _Invocation, [Expr]) ->
 	io:format("~s",[erlv8_vm:to_detail_string(VM,Expr)]),
 	undefined.
+
+reload(#erlv8_fun_invocation{} = _Invocation, []) ->
+	?V8Arr(lists:map(fun ({_,_}=O) -> ?V8Obj([O]) end,reloader:reload_modules(reloader:all_changed()))).
