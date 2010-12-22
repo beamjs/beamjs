4b0dbd02905e660d008737f303b547f3a8086940
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-22 12:37:03 -0800

    Implemented console.dir(object)

--- src/beamjs_mod_console.erl
+++ src/beamjs_mod_console.erl
@@ -9,9 +9,16 @@ init(_VM) ->
 exports(_VM) ->
 	%% TODO: time, timeEnd, trace, assert
 	erlv8_object:new([{"__doc__", "CommonJS [Console](http://wiki.commonjs.org/wiki/Console) Implementation"},
-			{"log", fun log/2},{"info", fun log/2},{"warn", fun log/2},{"error", fun log/2},{"dir", fun log/2}]).
+			{"log", fun log/2},{"info", fun log/2},{"warn", fun log/2},{"error", fun log/2},{"dir", fun dir/2}]).
 
 log(#erlv8_fun_invocation{ vm = VM } = _Invocation, [Expr]) ->
 	io:format("~s~n",[beamjs_js_formatter:format(VM,Expr)]),
 	undefined.
+
+dir(#erlv8_fun_invocation{ vm = VM } = _Invocation, [#erlv8_object{}=Expr]) ->
+	lists:foreach(fun ({Prop, Value}) ->
+						  io:format("  ~s: ~s~n",[Prop,beamjs_js_formatter:format(VM,Value)])
+				  end, Expr:proplist());
+dir(#erlv8_fun_invocation{} = Invocation, [#erlv8_fun{}=Expr]) ->
+	dir(Invocation, [Expr:object()]).
 
