d9b3138cf5df3e5c62a6fdecf187e55c3652edb8
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-22 13:10:55 -0800

    Store module exports in a ets database and also use it for detecting dependency cycles

--- src/beamjs_mod_require.erl
+++ src/beamjs_mod_require.erl
@@ -4,7 +4,8 @@
 -behaviour(erlv8_module).
 -include_lib("erlv8/include/erlv8.hrl").
 
-init(_VM) ->
+init(VM) ->
+	erlv8_vm:stor(VM,{beamjs_mod_require,mod_tab},ets:new(require_modules,[public,{heir, VM, none}])),
 	ok.
 
 exports(VM) ->
@@ -68,38 +69,59 @@ require_file(#erlv8_fun_invocation{ vm = VM } = Invocation, Filename) ->
 		[] ->
 			{throw, {error, lists:flatten(io_lib:format("Cannot find module '~s'",[Filename])) }};
 		[{Path,LoadedFilename,S}|_] ->
-			NewCtx = erlv8_context:new(VM),
-			NewGlobal = erlv8_context:global(NewCtx),
-			lists:foreach(fun ({K,V}) ->
-								  NewGlobal:set_value(K,V)
-						  end,  Global:proplist()),
-			NewGlobal:set_value("require",fun require/2),
-			NewRequire = NewGlobal:get_value("require"),
-			lists:foreach(fun ({K,V}) ->
-								  NewRequire:set_value(K,V)
-						  end,  Require:proplist()),
-			NewGlobal:set_value("module",?V8Obj([])),
-			Module = NewGlobal:get_value("module"),
-			Module:set_value("id", Filename, [dontdelete,readonly]),
-			NewGlobal:set_value("exports",?V8Obj([])),
-			NewGlobal:set_value("__dirname",Path),
-			NewGlobal:set_value("__filename",filename:join([Path,LoadedFilename])),
-			case erlv8_vm:run(VM,NewCtx,S,{LoadedFilename,0,0}) of
-				{ok, _} ->
-					lists:foreach(fun ({"exports",_}) ->
-										  ignore;
-									  ({"__dirname",_}) ->
-										  ignore;
-									  ({"__filename",_}) ->
-										  ignore;
-									  ({"module",_}) ->
-										  ignore;
-									  ({K,V}) ->
-										  Global:set_value(K,V)
-								  end,  NewGlobal:proplist()),
-					NewGlobal:get_value("exports");
-				{_,E} ->
-					{throw, {error, E}}
+			Tab = erlv8_vm:retr(VM,{beamjs_mod_require,mod_tab}),
+
+			case ets:lookup(Tab,Filename) of
+				[{Filename, loading}] ->
+					Module = Global:get_value("module"),
+					ModuleId = Module:get_value("id"),
+					ets:delete(Tab,Filename),
+					ets:delete(Tab,ModuleId),
+					{throw, {error, lists:flatten(io_lib:format(
+													"Dependency cycle detected while attempting to load '~s' from '~s'",
+													[Filename, ModuleId]))}};
+				[{Filename, Exports}] ->
+					io:format("cached already: ~p~n",[Exports]),
+					Exports;
+				[] ->
+					NewCtx = erlv8_context:new(VM),
+					NewGlobal = erlv8_context:global(NewCtx),
+					lists:foreach(fun ({K,V}) ->
+										  NewGlobal:set_value(K,V)
+								  end,  Global:proplist()),
+					NewGlobal:set_value("require",fun require/2),
+					NewRequire = NewGlobal:get_value("require"),
+					lists:foreach(fun ({K,V}) ->
+										  NewRequire:set_value(K,V)
+								  end,  Require:proplist()),
+					NewGlobal:set_value("module",?V8Obj([])),
+					Module = NewGlobal:get_value("module"),
+					Module:set_value("id", Filename, [dontdelete,readonly]),
+					NewGlobal:set_value("exports",?V8Obj([])),
+					NewGlobal:set_value("__dirname",Path),
+					NewGlobal:set_value("__filename",filename:join([Path,LoadedFilename])),
+
+					ets:insert(Tab,{Filename, loading}),
+
+					case erlv8_vm:run(VM,NewCtx,S,{LoadedFilename,0,0}) of
+						{ok, _} ->
+							lists:foreach(fun ({"exports",_}) ->
+												  ignore;
+											  ({"__dirname",_}) ->
+												  ignore;
+											  ({"__filename",_}) ->
+												  ignore;
+											  ({"module",_}) ->
+												  ignore;
+											  ({K,V}) ->
+												  Global:set_value(K,V)
+										  end,  NewGlobal:proplist()),
+							Exports = NewGlobal:get_value("exports"),
+							ets:insert(Tab, {Filename, Exports}),
+							Exports;
+						{_,E} ->
+							{throw, {error, E}}
+					end
 			end
 	end.
 	
