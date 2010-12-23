07994348686f40431c2cde42a840d16de9f6785e
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-23 06:35:29 -0800

    Added repl.quit() function.

--- src/beamjs_mod_repl.erl
+++ src/beamjs_mod_repl.erl
@@ -7,7 +7,8 @@ init(_VM) ->
 	ok.
 
 exports(_VM) ->
-	erlv8_object:new([{"start", fun start/2}]).
+	?V8Obj([{"start", fun start/2},
+			{"quit", fun quit/2}]).
 
 start(#erlv8_fun_invocation{} = Invocation, []) ->
 	case node() of
@@ -17,8 +18,19 @@ start(#erlv8_fun_invocation{} = Invocation, []) ->
 			start(Invocation,[lists:flatten(io_lib:format("(~p)beam.js> ",[Node]))])
 	end;
 			
-start(#erlv8_fun_invocation{ vm = VM } = _Invocation, [Prompt]) ->
-	supervisor:start_child(beamjs_repl_sup,[Prompt, beamjs_repl_console, VM]),
+start(#erlv8_fun_invocation{ vm = VM } = I, [Prompt]) ->
+	Ref = make_ref(),
+	{ok, Pid} = supervisor:start_child(beamjs_repl_sup,[Prompt, beamjs_repl_console, VM]),
+	erlv8_vm:stor(VM, {?MODULE, Ref}, self()),
+	Global = I:global(),
+	Global:set_hidden_value("replSessionRef", Ref),
 	receive X ->
+			exit(Pid),
 			X 
 	end.
+
+quit(#erlv8_fun_invocation{ vm = VM} = I, []) ->
+	Global = I:global(),
+	Ref = Global:get_hidden_value("replSessionRef"),
+	Pid = erlv8_vm:retr(VM, {?MODULE, Ref}),
+	Pid ! ok.
