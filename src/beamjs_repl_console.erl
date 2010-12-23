8014e1dbfae762bedef294ce24a02590c7a0adda
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-23 10:41:41 -0800

    REPL should not fail if expression evaluation caused an error

--- src/beamjs_repl_console.erl
+++ src/beamjs_repl_console.erl
@@ -5,9 +5,7 @@
 read(Prompt) ->
 	io:get_line(Prompt).
 
-print(VM,{compilation_failed, Error}) ->
-	io:format("~s~n",[beamjs_js_formatter:format_exception(VM,Error)]); 
 print(VM,{ok, Result}) ->
 	io:format("~s~n",[beamjs_js_formatter:format(VM,Result)]);
-print(VM,{exception, Exception}) ->
+print(VM,{throw, Exception}) ->
 	io:format("~s~n",[beamjs_js_formatter:format_exception(VM,Exception)]).
