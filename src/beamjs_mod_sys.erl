59676dde8d896dd5796661d1539334810ad31e99
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-23 01:41:14 -0800

    Added sys.inspect()

--- src/beamjs_mod_sys.erl
+++ src/beamjs_mod_sys.erl
@@ -9,12 +9,16 @@ init(_VM) ->
 exports(_VM) ->
 	{beamjs,_,Version} = lists:keyfind(beamjs,1,application:which_applications()),
 	?V8Obj([{"print", fun print/2},
+			{"inspect", fun inspect/2},
 			{"beamjs", ?V8Obj([{"version", Version},
 							   {"reload", fun reload/2}])}]).
 
 print(#erlv8_fun_invocation{ vm = VM} = _Invocation, [Expr]) ->
 	io:format("~s",[erlv8_vm:to_detail_string(VM,Expr)]),
 	undefined.
+
+inspect(#erlv8_fun_invocation{ vm = VM} = _Invocation, [Expr]) ->
+	lists:flatten(io_lib:format("~s",[beamjs_js_formatter:format(VM, Expr)])).
 
 reload(#erlv8_fun_invocation{} = _Invocation, []) ->
 	?V8Arr(lists:map(fun ({_,_}=O) -> ?V8Obj([O]) end,reloader:reload_modules(reloader:all_changed()))).
