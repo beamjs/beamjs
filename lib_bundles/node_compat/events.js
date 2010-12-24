8e5b760521ab2afe0d93476b32c8f6b499bb472d
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-24 14:42:44 -0800

    Reimplemented node_compat's events.EventEmitter in JS using erlang's gen_event

---
+++ lib_bundles/node_compat/events.js
@@ -0,0 +1,80 @@
+var gen_event = beamjs.bundles.with(['erlang'], function() { return require('gen_event'); });
+
+exports.EventEmitter = function() {
+	this._eventManager = new gen_event.Manager();
+	this._eventListeners = {};
+};
+
+addListener = function(event, listener) {
+ 	var that = this;
+	var f = function (arguments) {
+		args = Array.apply(this, arguments);
+		if (that._eventListeners[event].indexOf(listener) == -1)  {
+			this.removeHandler();
+		} else if (args.shift() == event) {
+			listener.apply(this,args);
+		}
+	};
+	f._original = listener;
+	this._eventManager.addHandler(new gen_event.Handler(f));
+	this.emit("newListener", event, listener);
+}
+
+
+exports.EventEmitter.prototype = {
+	emit: function () {
+		var _event = arguments[0];
+		// check _eventListeners consistency
+		var _handlers = this._eventManager.whichHandlers();
+		for (i in this._eventListeners[_event]) {
+			var _listener = this._eventListeners[_event][i];
+			var _found = false;
+			for (j in _handlers) {
+				var _handler = _handlers[j];
+				if ((_found = (_handler.onEvent._original == _listener)))
+					break;
+			}
+			if (!_found) {
+				addListener.call(this,_event,_listener);
+			}
+		}
+		// prepare event
+		args = [];
+		for (k in arguments) {
+			args[k] = arguments[k];
+		}
+		// send it off
+		this._eventManager.notify(args);
+	},
+	addListener: function(event, listener) {
+		if (this._eventListeners[event] == undefined)
+			this._eventListeners[event] = [];
+		this._eventListeners[event].push(listener);
+		addListener.call(this,event,listener);
+	},
+	once: function(event, listener) {
+		var that = this;
+		this.addListener(event, function () {
+							 listener.apply(this,args);
+							 that._eventListeners[event].splice(that._eventListeners[event].indexOf(listener),1);
+							 return this.RemoveHandler;
+						 });
+	},
+	listeners: function(event) {
+		if (this._eventListeners[event] == undefined)
+			this._eventListeners[event] = [];
+		return this._eventListeners[event];
+	},
+	removeListener: function(listener) {
+		for (event in this._eventListeners) {
+			if ((i=this._eventListeners[event].indexOf(listener)) > -1) {
+				this._eventListeners[event].splice(i,1);
+			}
+		}
+	},
+	removeAllListeners: function(event) {
+		this._eventListeners[event] = [];
+	}
+};
+
+exports.EventEmitter.prototype.on = exports.EventEmitter.prototype.addListener;
