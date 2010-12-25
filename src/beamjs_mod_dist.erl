86ba2fa91204f765002e5990002cc3d46442ddb4
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-24 16:26:15 -0800

    Started converting Erlang node management API to objects

--- src/beamjs_mod_dist.erl
+++ src/beamjs_mod_dist.erl
@@ -7,8 +7,12 @@
 init(_VM) ->
 	ok.
 
-exports(_VM) ->
+exports(VM) ->
+	Node = erlv8_vm:taint(VM, fun new_node/2),
+	Node:set_value("prototype", prototype_Node()),
+	erlv8_vm:stor(VM,{?MODULE, ctor_Node}, Node),
  	?V8Obj([{"node", fun node/2},
+			{"Node", Node},
 			{"ping", erlv8_fun:new(fun ping/2, 
 								   ?V8Obj([{"__doc__", 
 											"<code>ping(nodename)</code>\n\n"
@@ -21,13 +25,19 @@ exports(_VM) ->
 												 "the -name command line flag, it will return a fully qualified name."}]))},
 			{"nodes", fun nodes/2}]).
 
+prototype_Node() ->
+	?V8Obj([{"ping", fun ping/2}]).
 
 node(#erlv8_fun_invocation{},[]) ->
 	node().
 
+new_node(#erlv8_fun_invocation{ this = This },[Name]) when is_list(Name) ->
+	This:set_value("name", Name).
 
-ping(#erlv8_fun_invocation{},[Node]) when is_list(Node) ->
-	case net_adm:ping(list_to_atom(Node)) of
+
+ping(#erlv8_fun_invocation{ this = This },[]) ->
+	Node = list_to_atom(This:get_value("name")),
+	case net_adm:ping(Node) of
 		pong ->
 			true;
 		pang ->
@@ -37,5 +47,10 @@ ping(#erlv8_fun_invocation{},[Node]) when is_list(Node) ->
 localhost(#erlv8_fun_invocation{}, []) ->
 	net_adm:localhost().
 
-nodes(#erlv8_fun_invocation{},[]) ->
-	?V8Arr(nodes()).
+nodes(#erlv8_fun_invocation{ vm = VM },[]) ->
+	NodeCtor = erlv8_vm:retr(VM, {?MODULE, ctor_Node}),
+	?V8Arr(lists:map(fun (Node) ->
+							 NodeObj = NodeCtor:instantiate([atom_to_list(Node)]),
+							 NodeObj:set_prototype(prototype_Node()),
+							 NodeObj
+					 end, nodes())).
