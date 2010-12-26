018e8a4d47735f45b764f6e8cd101a5f68149e3e
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-12-26 21:06:49 +0200

    Node compat dns.reverse()

--- src/beamjs_mod_dns.erl
+++ src/beamjs_mod_dns.erl
@@ -19,22 +19,51 @@ exports(_VM) ->
                                 "        ips.forEach(function(ip) { console.log(ip) });\n"
                                 "    })"
                             }]))
+            },
+            {"reverse", erlv8_fun:new(fun reverse/2,
+                    ?V8Obj([{"__doc__",
+                                "`reverse(ip, callback)`\n\n"
+                                "Reverse resolves an ip address to an array of domain names.\n\n"
+                                "Example:\n\n"
+                                "    dns.resolve('174.129.212.2', function(err, names){\n"
+                                "        if (err) throw err;\n"
+                                "        names.forEach(function(name) { console.log(name) });\n"
+                                "    })"
+                            }]))
             }
         ]).
 
+%
+% Exposed Functions
+%
+
 resolve4(#erlv8_fun_invocation{},[Name, #erlv8_fun{} = Callback]) when is_list(Name) ->
-    case inet_res:resolve(Name, any, a) of
-        {ok, Msg} ->
-            { anlist, Records } = lists:keyfind(anlist, 1, inet_dns:msg(Msg)),
-            Ips = lists:map(fun ip_from_record/1, Records),
-            Callback:call([null, ?V8Arr(lists:map(fun ip4_to_string/1, Ips))]);
-        {error, _} ->
-            Callback:call([{error, "Cannot resolve"}, Name])
-    end.
+	case inet_res:resolve(Name, any, a) of
+		{ok, Msg} ->
+			{ anlist, Records } = lists:keyfind(anlist, 1, inet_dns:msg(Msg)),
+			Ips = lists:map(fun ip_from_record/1, Records),
+			Callback:call([null, ?V8Arr(lists:map(fun ip4_to_string/1, Ips))]);
+		{error, _} ->
+			Callback:call([{error, "Cannot resolve"}, Name])
+	end.
+
+reverse(#erlv8_fun_invocation{},[Ip, #erlv8_fun{} = Callback]) when is_list(Ip) ->
+	case inet:gethostbyaddr(Ip) of
+		{ok, Hostent} ->
+			{hostent, Address, _, _, _, _} = Hostent,
+			Callback:call([null, ?V8Arr([Address])]);
+		{error, _} ->
+			Callback:call([{error, "Cannot reverse resolve"}, Ip])
+	end.
 
 %
 % Utility Functions
 %
+
+% TODO
+erlerr2v8err(Err) ->
+	 Err.
+
 ip_from_record(Record) ->
     {_, Ip} = lists:keyfind(data, 1, inet_dns:rr(Record)),
     Ip.
