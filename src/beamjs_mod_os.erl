d94b65a754e4951751f671a867b89b1616e5ec70
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-12-28 17:37:19 +0200

    beamjs_mod_os:get_full_env/2 + some JS function naming fixes

--- src/beamjs_mod_os.erl
+++ src/beamjs_mod_os.erl
@@ -20,17 +20,21 @@ exports(_VM) ->
 						"`cmd(name)` -> String\n\n"
 						"Executes Command in a command shell of the target OS, "
 						"captures the standard output of the command and returns this result as a string."}]))},
-			{"find_executable", erlv8_fun:new(fun find_executable/2,
+			{"findExecutable", erlv8_fun:new(fun find_executable/2,
 					?V8Obj([{"__doc__",
-						"`find_executable(name[, path])` -> String\n\n"
+						"`findExecutable(name[, path])` -> String\n\n"
 						"These function look up an executable program given its name and a search path, in the same way as the underlying operating system.\n\n"
 						"The absolute filename of the executable program Name is returned, or false if the program was not found."}]))},
-			{"getenv", erlv8_fun:new(fun getenv/2,
+			{"getEnv", erlv8_fun:new(fun getenv/2,
 					?V8Obj([{"__doc__",
 						"`getenv(varname)` -> String\n\n"
-						"Returns the Value of the environment variable VarName.\n\n"
+						"Returns the Value of the environment variable `varname`.\n\n"
 						"Returns `false` if the environment variable is undefined."}]))},
-			{"putenv", erlv8_fun:new(fun putenv/2,
+			{"getFullEnv", erlv8_fun:new(fun getfullenv/2,
+					?V8Obj([{"__doc__",
+						"`getFullEnv()` -> Object\n\n"
+						"Returns the object containng all environment variables."}]))},
+			{"putEnv", erlv8_fun:new(fun putenv/2,
 					?V8Obj([{"__doc__",
 						"`putenv(varname, value)` -> true\n\n"
 						"Sets a new Value for the environment variable VarName."}]))},
@@ -52,9 +56,11 @@ find_executable(#erlv8_fun_invocation{},[Command, Path]) when is_list(Command) a
 find_executable(#erlv8_fun_invocation{},[Command]) when is_list(Command) ->
 	os:find_executable(Command).
 
-
 getenv(#erlv8_fun_invocation{},[VarName]) when is_list(VarName) ->
 	os:getenv(VarName).
+
+getfullenv(#erlv8_fun_invocation{},[]) ->
+	?V8Obj(lists:map(fun split_env_var/1, os:getenv())).
 
 putenv(#erlv8_fun_invocation{},[VarName, Value]) when is_list(VarName) ->
 	os:putenv(VarName, Value).
@@ -66,3 +72,10 @@ type(#erlv8_fun_invocation{},[]) ->
 version(#erlv8_fun_invocation{},[]) ->
 	{Major, Minor, Release} = os:version(),
 	?V8Arr([Major, Minor, Release]).
+
+% Utility functions
+split_env_var(VarStr) ->
+	Tokens = string:tokens(VarStr, "="),
+	Name   = lists:nth(1, Tokens),
+	Value  = string:join(lists:delete(Name, Tokens), "="),
+	{ Name, Value }.
