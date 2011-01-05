46d150437e531927f325afa6ef2fa73d2e7721ec
Yurii Rashkovskii <yrashk@gmail.com>
2011-01-04 23:37:55 -0800

    Further experimental (and a little bit ugly) support for Atoms and Tuples in direct Erlang API

--- src/beamjs_mod_erlang.erl
+++ src/beamjs_mod_erlang.erl
@@ -6,33 +6,73 @@
 init(_VM) ->
 	ok.
 
-exports(_VM) ->
-	?V8Obj([{"apply", fun erlang_apply/2}]).
+exports(VM) ->
+	Atom = erlv8_vm:taint(VM, fun new_Atom/2),
+	erlv8_vm:stor(VM, {?MODULE, atom}, Atom),
+	Tuple = erlv8_vm:taint(VM, fun new_Tuple/2),
+	erlv8_vm:stor(VM, {?MODULE, tuple}, Tuple),
+	?V8Obj([{"apply", fun erlang_apply/2},
+			{"Atom", Atom}, {"Tuple", Tuple}
+		   ]).
 
-erlang_apply(#erlv8_fun_invocation{}, [Module, Fun, #erlv8_array{} = Args]) when is_list(Module) andalso is_list(Fun) ->
-	from_native(erlang:apply(list_to_atom(Module), list_to_atom(Fun), to_native(Args))).
-	
-to_native(#erlv8_object{}=O) ->
-	lists:map(fun ({K,V}) ->
-					  {K, to_native(V)}
-			  end, O:proplist());
-to_native(#erlv8_array{}=A) ->
-	lists:map(fun to_native/1, A:list());
-to_native(Other) ->
+new_Atom(#erlv8_fun_invocation{ this = This }, [Atom]) when is_list(Atom) ->
+	This:set_value("atom", Atom).
+
+new_Tuple(#erlv8_fun_invocation{ this = This }, [#erlv8_array{}=Array]) ->
+	This:set_value("tuple", Array).
+
+erlang_apply(#erlv8_fun_invocation{ vm = VM }, [Module, Fun, #erlv8_array{} = Args]) when is_list(Module) andalso is_list(Fun) ->
+	from_native(VM, erlang:apply(list_to_atom(Module), list_to_atom(Fun), to_native(VM, Args))).
+
+-define(Is(O,X),  ((O:get_prototype()):get_value("constructor")):equals(erlv8_vm:retr(VM, {?MODULE, X}))).
+
+to_native(VM, #erlv8_object{}=O) ->
+	case ?Is(O,atom) of
+		true ->
+			list_to_atom(O:get_value("atom"));
+		false ->
+			case ?Is(O,tuple) of
+				true ->
+					list_to_tuple(lists:map(fun (V) -> to_native(VM, V) end, (O:get_value("tuple")):list()));
+				false ->
+					lists:map(fun ({K,V}) ->
+									  {K, to_native(VM, V)}
+							  end, O:proplist())
+			end
+	end;
+
+to_native(VM, #erlv8_array{}=A) ->
+	lists:map(fun (V) -> to_native(VM, V) end, A:list());
+
+to_native(_VM, Other) ->
 	Other.
 
-from_native(T) when is_tuple(T) ->
-	?V8Arr(tuple_to_list(T));
-from_native(L) when is_list(L) ->
+from_native(VM, T) when is_tuple(T) ->
+	L = tuple_to_list(T),
+	TupleCtor = erlv8_vm:retr(VM, {?MODULE, tuple}),
+	TupleCtor:instantiate([?V8Arr(lists:map(fun (V) -> from_native(VM, V) end, L))]);
+
+from_native(VM, A) when is_atom(A) ->
+	S = atom_to_list(A),
+	AtomCtor = erlv8_vm:retr(VM, {?MODULE, atom}),
+	AtomCtor:instantiate([S]);
+	
+from_native(VM, L) when is_list(L) ->
 	case is_simple_proplist(L) of
 		true ->
 			?V8Obj(lists:map(fun ({K,V}) ->
-									 {K, from_native(V)}
+									 {K, from_native(VM, V)}
 							 end, L));
 		false ->
-			?V8Arr(lists:map(fun from_native/1, L))
+			case io_lib:printable_list(L) of
+				true ->
+					L;
+				false ->
+					?V8Arr(lists:map(fun (V) -> from_native(VM, V) end, L))
+			end
 	end;
-from_native(Other) ->
+
+from_native(_VM, Other) ->
 	Other.
 
 	
