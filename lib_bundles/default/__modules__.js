3f44548f77e9e5ef01957a59c743483cfa0215f2
Yurii Rashkovskii <yrashk@gmail.com>
2011-01-05 17:46:01 -0800

    Experimental support for tick enqueueing and frontal enqueueing (nextTick)

--- lib_bundles/default/__modules__.js
+++ lib_bundles/default/__modules__.js
@@ -1,1 +1,2 @@
-exports = { repl: require({module: "beamjs_mod_repl"}) }
+exports = { repl: require({module: "beamjs_mod_repl"}),
+		    erlv8: require({module: "beamjs_mod_erlv8"})};
---
+++ src/beamjs_mod_erlv8.erl
@@ -0,0 +1,21 @@
+-module(beamjs_mod_erlv8).
+-behaviour(beamjs_module).
+-export([init/1,exports/1]).
+-include_lib("erlv8/include/erlv8.hrl").
+
+init(_VM) ->
+	ok.
+
+exports(_VM) ->
+	?V8Obj([{"nextTick",fun next_tick/2},
+			{"enqueueTick", fun enqueue_tick/2}
+		   ]).
+
+next_tick(#erlv8_fun_invocation{ vm = VM },[#erlv8_fun{ resource = R }=F]) ->
+	spawn(fun () -> erlv8_vm:next_tick(VM, {call, R, []}) end),
+	F.
+
+enqueue_tick(#erlv8_fun_invocation{ vm = VM },[#erlv8_fun{ resource = R }=F]) ->
+	spawn(fun () -> erlv8_vm:enqueue_tick(VM, {call, R, []}) end),
+	F.
+	
